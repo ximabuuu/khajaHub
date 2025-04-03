c73965f2a7026bfbc592388fd02704098aaa44d6
Shuvam Shrestha <shuvamofficial6@gmail.com>
2025-04-03 16:30:55 +0545

    khai k vo vo

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,22 @@
-  import { defineConfig } from 'vite'
-  import react from '@vitejs/plugin-react'
-  import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-  // https://vite.dev/config/
-  export default defineConfig({
-    plugins: [
-      react(),
-      tailwindcss()
-    ],
-    resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],  // Add extensions you want Vite to resolve automatically
-    },
-    build: {
-      rollupOptions: {
-        external: ['mongoose'],  // Exclude mongoose from frontend bundling
-      }
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],  // Add extensions you want Vite to resolve automatically
+  },
+  optimizeDeps: {
+    exclude: ['mongoose'], // Ensure mongoose is excluded
+  },
+  build: {
+    rollupOptions: {
+      external: ['mongoose'], // Exclude mongoose from frontend bundling
     }
-  })
+  }
+})
