a3711ff9ae801c022a13ec3c440454a7b0ba683b
Shuvam Shrestha <shuvamofficial6@gmail.com>
2025-04-03 16:02:00 +0545

    loading

--- client/src/component/ProdCardByCate.jsx
+++ client/src/component/ProdCardByCate.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const ProdCardByCate = () => {
   return (
     <div className='border border-blue-100 bg-white shadow-md hover:-translate-y-2.5 transition- py-2 lg:p-4 grid lg:gap-3 gap-1 mx-auto min-w-36 max-w-36 lg:max-w-52 lg:min-w-52 rounded cursor-pointer animate-pulse '>
-      <div className='min-h-24 lg:min-h-20 max-w-36 lg:max-w-43 bg-blue-50 rounded'>
-
+      <div className='min-h-24 lg:min-h-20 max-w-36 lg:max-w-43 bg-blue-50 rounded flex items-center justify-center'>
+        <p className='text-gray-400'>Khaja</p>
       </div>
       <div className='p-2 lg:p-3 h-8 bg-blue-50 rounded w-20'>
 
